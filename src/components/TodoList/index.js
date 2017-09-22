a4d20107af6d09421ebea7cbaf6e9e4abe2e1c78
Antoine JACKSON <a.jackson@meetic-corp.com>
2017-09-22 16:15:03 +0200

    feat(state-management): add redux

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,16 +1,60 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
 
 import TodoItem from 'components/TodoItem';
 
-export default class TodoList extends Component {
+export class TodoList extends Component {
+  state = {
+    todoName: ''
+  };
+  setLocalTodo(name) {
+    this.setState({
+      todoName: name
+    });
+  }
+  addTodo() {
+    if (this.state.todoName !== '') {
+      this.props.dispatch({
+        type: 'ADD_TODO',
+        payload: this.state.todoName
+      });
+      this.setState({
+        todoName: ''
+      });
+    }
+  }
+  renderTodos() {
+    return this.props.todolist.map((todo, index) =>
+      <TodoItem key={index} content={todo} />
+    );
+  }
   render() {
     return (
-      <ul>
-        <TodoItem content="Todo 1" />
-        <TodoItem content="Todo 2" />
-        <TodoItem content="Todo 3" />
-        <TodoItem content="Todo 4" />
-      </ul>
+      <div>
+        <ul>
+          {this.renderTodos()}
+        </ul>
+        <form action="javascript:void(0);">
+          <input
+           onChange={e => this.setLocalTodo(e.target.value)}
+           type="text"
+           placeholder="Enter a todo name"
+           value={this.state.todoName}/>
+          <button
+           onClick={e => this.addTodo()}
+           type="submit"
+           disabled={this.state.todoName === ''}>
+            Add todo
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+export default connect(state => {
+  return {
+    todolist: state.todos
+  };
+})(TodoList);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,32 @@ import reactDom from 'react-dom';
 
 import TodoList from 'components/TodoList';
 
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState ={
+  todos: ['My todo', 'Other todo']
+};
+
+const store = createStore((state = initialState, {type, payload}) => {
+  switch(type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          payload
+        ]
+      }
+    default:
+      return state;
+  }
+});
+
+
 reactDom.render(
-  <div>
-  <TodoList />
-  </div>,
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
   document.getElementById('root')
 );
